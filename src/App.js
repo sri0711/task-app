70b45c2e0e80c77a2b1d2b6736158cb535ac6a42
padhudev <webdev.royalgroup@gmail.com>
2021-09-19 15:42:51 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Datas from './Datas';
 import './App.css';
 
+const task = [];
+let id = -1;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const d = new Date();
+	const [Data, setData] = useState(task);
+	const Add = (event) => {
+		event.preventDefault();
+		id = id + 1;
+		const msg = event.target.form.add.value;
+		const pushdata = {
+			id: id,
+			msg: msg,
+			date: `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`,
+			time: `${d.getHours()}:${d.getMinutes()}`
+		};
+
+		setData([...Data, pushdata]);
+		event.target.form.add.value = '';
+	};
+	return (
+		<div className='App'>
+			<form>
+				<input type='text' name='add' className='input' />
+				<input type='submit' value='Add Event' onClick={Add} />
+			</form>
+			<div className='task'>
+				<Datas data={Data} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Datas.js
@@ -0,0 +1,73 @@
+import './datas.css';
+
+function Datas(props) {
+	const editFunc = (event) => {
+		const data = event;
+		const h1 = document.getElementById(`ms${data}`);
+		const frm = document.getElementById(`fr${data}`);
+		h1.style = 'display:none;';
+		frm.style = 'display:flex;';
+	};
+	const delFunc = (event) => {
+		const data = event;
+		const index = props.data[data];
+		props.data.splice(index, data);
+		const temp = document.getElementById(`el${data}`);
+		temp.remove();
+	};
+
+	return (
+		<div className='render'>
+			{props.data.map((task) => (
+				<div className='main' id={`el${task.id}`}>
+					<div>
+						<div className='msg'>
+							<h1>{task.id}</h1>
+							<h1 id={`ms${task.id}`}>{task.msg}</h1>
+							<form style={{ display: 'none' }} id={`fr${task.id}`}>
+								<input type='text' name='emsg' />
+								<input
+									type='button'
+									value='save'
+									onClick={(event) => {
+										event.preventDefault();
+										const edmsg = event.target.form.emsg.value;
+										const h1msg = document.getElementById(
+											`ms${task.id}`
+										);
+										h1msg.innerText = edmsg;
+										h1msg.style = 'display:flex';
+										document.getElementById(`fr${task.id}`).style =
+											'display:none';
+									}}
+								/>
+							</form>
+						</div>
+						<div className='time'>
+							<p>{task.date}</p>
+							<p>{task.time}</p>
+						</div>
+					</div>
+					<div className='options'>
+						<button
+							onClick={() => {
+								editFunc(task.id);
+							}}
+						>
+							edit
+						</button>
+						<button
+							onClick={() => {
+								delFunc(task.id);
+							}}
+						>
+							delete
+						</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default Datas;
